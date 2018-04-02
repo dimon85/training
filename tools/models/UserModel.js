4ffcaac13421209d81c87700b4d074f14fda9138
dimon85 <glimor800@gmail.com>
2018-04-02 18:06:23 +0300

    update user model

--- tools/models/UserModel.js
+++ tools/models/UserModel.js
@@ -4,10 +4,10 @@ import bcrypt from 'bcrypt';
 
 
 // Use native promises
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-const userSchema = new Schema({
-  username: {
+const UserSchema = new Schema({
+  email: {
     type: String,
     required: true,
     unique: true,
@@ -30,36 +30,57 @@ const userSchema = new Schema({
 });
 
 // authenticate user login input against db document
-userSchema.statics.authenticate = function (username, password) {
-  return new Promise((resolve, reject) => {
-    this.model('user').findOne({username})
-      .exec()
-      .then((user)=>{
-        if(user){
-          bcrypt.compare(password, user.password, function (err, match) {
-            // password matches hashed password from db
-            if(err) return reject(err);
-            match ? resolve(user) : function(){
-              const err = new Error('Invalid password');
-              // although i wouldn't differentiate
-              // username error from pass error for the sake of security
-              err.status = 401;
-              reject(err);
-            }();
-          });
-        } else {
-          const err = new Error('User not found');
-          err.status = 401;
-          reject(err);
-        }
-      })
-      .catch((err)=> reject(err));
-  })
+UserSchema.statics = {
+  /**
+   * Check auth for user
+   * @param {string} email - The email of user.
+   * @param {string} password - The password of user.
+   * @returns {Promise<User, APIError>}
+   */
+  authenticate(email, password) {
+    console.log('-****-', email, password)
 
+    return this.findOne({ email }).exec()
+      .then(((user) => {
+        console.log('USER', user);
+        return user;
+      }))
+      .catch((err) => {
+        console.log('ERROR',)
+        throw err;
+      });
+
+    // return this.model('users')
+    // return new Promise((resolve, reject) => {
+    //   console.log('USERs', this);
+    //   this.model('users').findOne({ email }).exec()
+    //     .then((user) => {
+    //       console.log('USER', user);
+    //       if (user) {
+    //         bcrypt.compare(password, user.password, function (err, match) {
+    //           // password matches hashed password from db
+    //           if (err) return reject(err);
+    //           match ? resolve(user) : function(){
+    //             const err = new Error('Invalid password');
+    //             // although i wouldn't differentiate
+    //             // username error from pass error for the sake of security
+    //             err.status = 401;
+    //             reject(err);
+    //           }();
+    //         });
+    //       } else {
+    //         const err = new Error('User not found');
+    //         err.status = 401;
+    //         reject(err);
+    //       }
+    //     })
+    //     .catch((err)=> reject(err));
+    // })
+  }
 };
 
 // hash and salt password before saving to db
-userSchema.pre("save", function(next) { // !!! this and arrow function
+UserSchema.pre('save', function(next) { // !!! this and arrow function
   bcrypt.hash(this.password, 10)
     .then((hash) => {
       console.log('new hash', hash);
@@ -68,14 +89,20 @@ userSchema.pre("save", function(next) { // !!! this and arrow function
     }).catch( err => next(err));
 });
 
-userSchema.methods.publicFormat = function() {
-  const result = this.toJSON();
-  // this is to get rid of the  __v
-  delete(result.__v);
-  delete result.password;
-  return result
-};
+/**
+ * Methods
+ */
+UserSchema.method({
+  publicFormat() {
+    const result = this.toJSON();
+    // this is to get rid of the  __v
+    delete(result.__v);
+    delete result.password;
+    return result
+  }
+});
 
-const User = mongoose.model('user', userSchema );
-
-module.exports = User;
+/**
+ * @typedef users
+ */
+module.exports = mongoose.model('users', UserSchema);
